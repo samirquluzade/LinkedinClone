4eb38601de95af4bfa1cb58e947396e5373c0dfc
SAMIR QULUZADEH <samir.guluzadeh@vabiss.com>
2021-11-25 13:36:56 +0400

    Created: (Home and Header Component)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
 import {Route,Routes} from "react-router-dom";
+
 import Login from './components/Login';
+import Header from './components/Header';
+import Home from './components/Home';
 
 
 function App() {
   return (
     <div className="App">
         <Routes>
-          <Route path='/' element={<Login />}/>
+          <Route exact path='/' element={<Login />}/>
+          <Route exact path='/home' element={[<Header />,<Home />]}/>
         </Routes>
     </div>
   );
