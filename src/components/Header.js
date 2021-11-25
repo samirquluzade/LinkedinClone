f90ad11439e486f81a5349d1e54c9ec059610b8a
SAMIR QULUZADEH <samir.guluzadeh@vabiss.com>
2021-11-25 13:47:50 +0400

    Created: (Home and Header Components and Styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,14 @@ const Header = (props) => {
                         <img src="/images/home-logo.svg" alt="" />
                     </a>
                 </Logo>
+                <Search>
+                    <div>
+                        <input type="text" placeholder="Search" />
+                    </div>
+                    <SearchIcon>
+                        <img src="/images/search-icon.svg" alt="" />
+                    </SearchIcon>
+                </Search>
             </Content>
         </Container>
     )
@@ -38,4 +46,39 @@ const Logo = styled.span`
   font-size: 0px;
 `;
 
+const Search = styled.div`
+  opacity: 1;
+  flex-grow: 1;
+  position: relative;
+  & > div{
+    max-width: 280px;
+    input{
+      border: none;
+      box-shadow: none;
+      background-color: #eef3f8;
+      border-radius: 2px;
+      color:rgba(0,0,0,0.9);
+      width: 218px;
+      padding:0 8px 0 40px;
+      line-height: 1.75;
+      font-weight: 400;
+      font-size: 14px;
+      height: 34px;
+      border-color: #dce6f1;
+      vertical-align: text-top;
+    }
+  }
+`;
+
+const SearchIcon = styled.div`
+  width: 40px;
+  position: absolute;
+  top:10px;
+  left:2px;
+  border-radius: 0 2px 2px 0;
+  margin:0;
+  pointer-events: none;
+  display: flex;
+  justify-content: center;
+`;
 export default Header;
