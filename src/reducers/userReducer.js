c06a64dd6150535f1f4cdb1255cf2f068d43c96f
SAMIR QULUZADEH <samir.guluzadeh@vabiss.com>
2021-11-26 12:51:05 +0400

    Created: (Home page structure,styles and redux setup

---
+++ src/reducers/userReducer.js
@@ -0,0 +1,12 @@
+const INITITAL_STATE = {
+    user:null
+}
+
+const userReducer = (state = INITITAL_STATE,action) => {
+    switch (action.type){
+        default:
+            return state;
+    }
+}
+
+export default userReducer;
