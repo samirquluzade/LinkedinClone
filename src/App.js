1fa33f92fe02e60a8a365b886c9f2caba9456477
SAMIR QULUZADEH <samir.guluzadeh@vabiss.com>
2021-11-26 16:36:30 +0400

    Created: (Save active user details to redux)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import {Route,Routes} from "react-router-dom";
+import {connect} from 'react-redux';
 
 import Login from './components/Login';
 import Header from './components/Header';
 import Home from './components/Home';
+import {getUserAuth} from "./actions";
 
 
-function App() {
+function App(props) {
+    useEffect(() => {
+        props.getUserAuth();
+    },[]);
   return (
     <div className="App">
         <Routes>
@@ -18,4 +23,12 @@ function App() {
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+    return {};
+}
+
+const mapDispatchToProps = (dispatch) => ({
+    getUserAuth: () => dispatch(getUserAuth())
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(App);
