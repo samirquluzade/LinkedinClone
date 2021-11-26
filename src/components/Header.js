b8c7f0e121d948d0ab7daa2fb6fd93aa628d1619
SAMIR QULUZADEH <samir.guluzadeh@vabiss.com>
2021-11-26 17:27:21 +0400

    Created: (Show active user profile picture)

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,7 @@
 import styled from 'styled-components';
 
+import {connect} from 'react-redux';
+
 const Header = (props) => {
     return(
         <Container>
@@ -56,7 +58,8 @@ const Header = (props) => {
 
                         <User>
                             <a>
-                                <img src="/images/user.svg" alt="" />
+                                {props.user && props.user.photoURL ? <img src={props.user.photoURL} alt=""/> :
+                                    <img src="/images/user.svg" alt="" />}
                                 <span>Me</span>
                                 <img src="/images/down-icon.svg" alt="" />
                             </a>
@@ -247,4 +250,14 @@ const User = styled(NavList)`
 const Work = styled(User)`
   border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
-export default Header;
+
+const mapStateToProps = (state) => {
+    return{
+        user: state.userState.user
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+
+}
+export default connect(mapStateToProps,mapDispatchToProps)(Header);
